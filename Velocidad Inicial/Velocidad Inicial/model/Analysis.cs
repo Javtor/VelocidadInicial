5231bdfd751a2764d83ec1b3f40aa3671f47e77c
Alejandro Barrera <alejobarre9@gmail.com>
2019-02-18 22:21:34 -0500

    UML corrections

---
+++ Velocidad Inicial/Velocidad Inicial/model/Analysis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velocidad_Inicial.model
+{
+    class Analysis
+    {
+        public int TIME = 1;
+        public int ANGLE = 2;
+        public int DISTANCE = 3;
+        public List<Register> registers;
+
+        public void ReadCsv()
+        {
+
+        }
+
+        public double AverageVo1()
+        {
+            return -1;
+        }
+
+        public double AverageVo2()
+        {
+            return -1;
+        }
+
+        public double CalculateUncertainty()
+        {
+            return -1;
+        }
+        
+    }
+}
---
+++ Velocidad Inicial/Velocidad Inicial/model/Register.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velocidad_Inicial.model
+{
+    class Register
+    {
+        public double GRAVITY = 9.8;
+        private double Time { get; set; }
+        private double Angle { get; set; }
+        private double Distance { get; set; }
+        private double Vo1 { get; set; }
+        private double Vo2 { get; set; }
+
+        public Register(int time, int angle, int distance)
+        {
+            Time = time;
+            Angle = angle;
+            Distance = distance;
+        }
+
+        public double CalculateVo1()
+        {
+            return -1;
+        }
+
+        public double CalculateVo2()
+        {
+            return -1;
+        }
+
+
+
+    }
+}
